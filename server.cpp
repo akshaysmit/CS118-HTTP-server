e302b447a46a55a125c623c46f4d760ee0e4f3cc
Nikola Samardzic <nikola.s@ucla.edu>
2018-01-27 12:38:41 -0800

    Added header to server.c, added README

--- server.cpp
+++ server.cpp
@@ -1,3 +1,12 @@
+/******************************************************************************
+ * CS 118, Winter 2018 - Project 1
+ * -------------------------------
+ * Authors: 
+ *     - Akshay Smit
+ *     - Nikola Samardzic
+ * 
+ *****************************************************************************/
+
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -11,72 +20,70 @@
 #define BUFSIZE 512    //Change this to modify client input buffer size
 
 int main(int argc, char **argv) {
-  int sockfd, portno;
-  socklen_t clilen;
-  struct sockaddr_in serv_addr, cli_addr;
+    int sockfd, portno;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
 
-  if (argc < 2) {
-    fprintf(stderr,"Please provide port number\n");
-    exit(1);
-  }
-  else
-    portno = atoi(argv[1]);
+    if (argc < 2) {
+        fprintf(stderr,"Please provide port number\n");
+        exit(1);
+    }
+    else
+        portno = atoi(argv[1]);
   
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;          //Code of address family
-  serv_addr.sin_addr.s_addr = INADDR_ANY;  //IP address of this machine
-  serv_addr.sin_port = htons(portno);      //Port number in network byte order 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;          //Code of address family
+    serv_addr.sin_addr.s_addr = INADDR_ANY;  //IP address of this machine
+    serv_addr.sin_port = htons(portno);      //Port number in network byte order 
 
-  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    perror("Error on binding: ");
-    exit(1);
-  }
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Error on binding: ");
+        exit(1);
+    }
 
-  fd_set active_fd_set, read_fd_set;
-  listen(sockfd, 1);
+    fd_set active_fd_set, read_fd_set;
+    listen(sockfd, 1);
  
-  /* Initialize active socket set */
-  FD_ZERO(&active_fd_set);
-  FD_SET(sockfd, &active_fd_set);
+    /* Initialize active socket set */
+    FD_ZERO(&active_fd_set);
+    FD_SET(sockfd, &active_fd_set);
   
-  while (1) {
-    read_fd_set = active_fd_set;
-    /* Wait until a socket is ready */
-    if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0) {
-      perror("Error with select: ");
-      exit(1);
+    while (1) {
+        read_fd_set = active_fd_set;
+        /* Wait until a socket is ready */
+        if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0) {
+            perror("Error with select: ");
+            exit(1);
+        }
+
+        /* Check sockets ready with input */
+        for (int i = 0; i < FD_SETSIZE; i++) {
+            if (FD_ISSET(i, &read_fd_set)) {
+
+                /* New connection request */
+                if (i == sockfd) {
+                    int newfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+                    if (newfd < 0) {
+                        perror("Error accepting connection: ");
+                        exit(1);
+                    }
+
+                    FD_SET(newfd, &active_fd_set);
+                }
+
+                /* Data from connected client */
+                else {
+                    char buf[BUFSIZE]; 
+                    int n_read = read(i, buf, BUFSIZE);
+                    write(1, buf, n_read); //Echo output
+                    
+                    //HTTP RESPONSE GOES HERE
+                    //YOU MUST WRITE TO FILE DESCRIPTOR i
+                    //CLIENT INPUT IS IN THE BUFFER buf
+                }
+            }
+        }
     }
-
-    /* Check sockets ready with input */
-    int i;
-    for (i = 0; i < FD_SETSIZE; i++) {
-      if (FD_ISSET(i, &read_fd_set)) {
-
-	/* New connection request */
-	if (i == sockfd) {
-
-	  int newfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	  if (newfd < 0) {
-	    perror("Error accepting connection: ");
-	    exit(1);
-	  }
-
-	  FD_SET(newfd, &active_fd_set);
-	}
-
-	/* Data from connected client */
-	else {
-	  char buf[BUFSIZE]; 
-	  int n_read = read(i, buf, BUFSIZE);
-	  write(1, buf, n_read); //Echo output
-
-	  //HTTP RESPONSE GOES HERE
-	  //YOU MUST WRITE TO FILE DESCRIPTOR i
-	  //CLIENT INPUT IS IN THE BUFFER buf
-	}
-      }
-    }
-  }
 }
 

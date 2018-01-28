7224ec992f703a1122e000d2faeade89a14cecfb
Nikola Samardzic <nikola.s@ucla.edu>
2018-01-27 16:30:28 -0800

    Sort of works

--- server.cpp
+++ server.cpp
@@ -7,6 +7,9 @@
  * 
  *****************************************************************************/
 
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -18,6 +21,51 @@
 #include <strings.h>
 
 #define BUFSIZE 512    //Change this to modify client input buffer size
+
+#include <iostream>
+using namespace std;
+
+// getmsg should be a full GET HTML message
+void respond(char* getmsg, int dest_socket) {
+    cout<< "getmsg: " << getmsg;
+    char doublebuff[2];
+    doublebuff[0] = getmsg[0];
+    doublebuff[1] = getmsg[1];
+    size_t index;
+    for(index = 0; !(doublebuff[0] == '\r' && doublebuff[1] == '\n');
+        index++) {
+        doublebuff[0] = getmsg[index];
+        doublebuff[1] = getmsg[index + 1];
+    }
+    cout << "index: " << index << endl << endl;
+    // Here index will point to index of '\r' in first occurance of "\r\n"
+    if (!(getmsg[0] == 'G' && getmsg[1] == 'E'
+          && getmsg[2] == 'T' && getmsg[3] == ' ' && getmsg[4] == '/')) {
+        fprintf(stderr,
+                "GET request does not start with keyword GET!\n");
+        exit(1);
+    }
+
+    char *filepath = (char*) malloc(index);
+    bzero(filepath, index);
+    for (int ci = 0; ci < index - 15; ci++) {
+        cout << ci+5 << " :: " << getmsg[ci+5] << endl;
+        filepath[ci] = getmsg[ci+5];
+    }
+    cout<< "filepath: " << filepath << "END" << endl;                      
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Error while opening file!\n");        
+        exit(1);
+    }
+    
+    write(dest_socket, "HTTP/1.1 200 OK\r\n\r\n", 23);
+
+    char c;    
+    for (read(fd, &c, 1); read(fd, &c, 1) > 0; )
+        write(dest_socket, &c, 1);
+    write(dest_socket, "\r\n\r\n", 4);
+}
 
 int main(int argc, char **argv) {
     int sockfd, portno;
@@ -74,13 +122,31 @@ int main(int argc, char **argv) {
 
                 /* Data from connected client */
                 else {
-                    char buf[BUFSIZE]; 
+                    size_t currsize = 1024;
+                    char *getmsg = (char*)malloc(currsize);
+                    bzero(getmsg, 1024);
+                    char buf[BUFSIZE];
+                    bzero(buf, BUFSIZE);
                     int n_read = read(i, buf, BUFSIZE);
                     write(1, buf, n_read); //Echo output
-                    
-                    //HTTP RESPONSE GOES HERE
-                    //YOU MUST WRITE TO FILE DESCRIPTOR i
-                    //CLIENT INPUT IS IN THE BUFFER buf
+                    size_t len = strlen(getmsg);
+                    for (size_t index = 0; index < n_read; index++) {
+                        getmsg[len] = buf[index];
+                        len++;
+                        if (len >= currsize) {
+                            currsize*=2;
+                            char *getmsg_realloc = (char*)realloc(getmsg, currsize);
+                            if (getmsg_realloc) {
+                                getmsg = getmsg_realloc;
+                            } else {
+                                fprintf(stderr,
+                                        "Memory allocation error!\n");
+                                exit(1);
+                            }
+                        }
+                    }
+                    bzero(buf, BUFSIZE);
+                    respond(getmsg, i);
                 }
             }
         }
